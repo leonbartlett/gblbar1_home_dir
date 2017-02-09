32291e72e38a34a1430d945e03069139050fbbe9
Leon Bartlett <leon.bartlett@atradius.com>
2017-02-09 09:40:53 +0000

    removed unused manifests, bash_profile and added bashrc

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,7 +21,6 @@ class gblbar1_home_dir {
     source  => 'puppet:///modules/gblbar1_home_dir/vim',
     recurse => true,
   }
-
   file { '/home/gblbar1/.gitconfig' :
     ensure => present,
     owner  => 'gblbar1',
@@ -36,16 +35,31 @@ class gblbar1_home_dir {
     mode   => '0664',
     source => 'puppet:///modules/gblbar1_home_dir/vimrc',
   }
-  file { '/home/gblbar1/.bash_profile' :
+  file { '/home/gblbar1/.bashrc' :
     ensure => present,
     owner  => 'gblbar1',
     group  => 'gblbar1',
     mode   => '0644',
-    source => 'puppet:///modules/gblbar1_home_dir/bash_profile',
+    source => 'puppet:///modules/gblbar1_home_dir/bashrc',
   }
-
-
-
-
+  file { '/home/gblbar1/puppet/modules' :
+    ensure => directory,
+    owner  => 'gblbar1',
+    group  => 'gblbar1',
+    mode   => '0655',
+  }
+  file { '/home/gblbar1/puppet/manifests' :
+    ensure => directory,
+    owner  => 'gblbar1',
+    group  => 'gblbar1',
+    mode   => '0755',
+  }
+  file { '/home/gblbar1/git_personal_access_tokens' :
+    ensure => present,
+    owner  => 'gblbar1',
+    group  => 'gblbar1',
+    mode   => '0400',
+    source => 'puppet:///modules/gblbar1_home_dir/git_personal_access_tokens',
+  }
 
 }
