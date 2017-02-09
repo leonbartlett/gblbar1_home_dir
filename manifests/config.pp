26635c61fdf351de45fae4ae83dbbe3a81a5b15b
Leon Bartlett <leon.bartlett@atradius.com>
2017-02-09 09:07:01 +0000

    added puppet modules directory

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,18 @@
 # This class is called from gblbar1_home_dir for service config.
 #
 class gblbar1_home_dir::config {
+  file { "/home/gblbar1/puppet/modules" :
+    ensure  => directory,
+    owner   => 'gblbar1',
+    group   => 'gblbar1',
+    mode    => "0655",
+  }
+  file { "/home/gblbar1/puppet/manifests" :
+    ensure  => directory,
+    owner   => "gblbar1",
+    group   => "gblbar1",
+    mode    => "0755",
+  }
+
+
 }
